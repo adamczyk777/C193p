4135c2356c1d4e88b3a38c7d8d1c4d122bc8914c
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2020-04-10 20:37:40 +0200

    add gitignore

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Jakub Adamczyk on 10/04/2020.
+//  Copyright © 2020 Jakub Adamczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
